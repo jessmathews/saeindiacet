90484a26d61b18f1b349e3cf334953223c2072a1
Naisel <naisel329@gmail.com>
2023-02-22 22:59:43 +0530

    events completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,7 +86,9 @@
     const headerScrolled = () => {
       if (window.scrollY > 100) {
         selectHeader.classList.add("header-scrolled");
-      } 
+      } else {
+        selectHeader.classList.remove("header-scrolled");
+      }
     }
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
